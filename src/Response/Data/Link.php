4ce254c6337665a0a4edd2cce66b5e30c8adf911
ArrayIterator <arrayiterator@gmail.com>
2025-01-03 19:21:57 +0700

    - code improvements - add doc comment phpstan & readabilities improvement

--- src/Response/Data/Link.php
+++ src/Response/Data/Link.php
@@ -4,13 +4,22 @@ declare(strict_types=1);
 namespace ArrayAccess\RdapClient\Response\Data;
 
 use ArrayAccess\RdapClient\Response\Data\Abstracts\AbstractRdapResponseDataRecursiveArray;
+use IteratorAggregate;
 use function array_filter;
-use function in_array;
 
-class Link extends AbstractRdapResponseDataRecursiveArray
+/**
+ * @template-implements IteratorAggregate<string, Value|Rel|Href|HrefLang|Title|Media|Type>
+ */
+class Link extends AbstractRdapResponseDataRecursiveArray implements IteratorAggregate
 {
+    /**
+     * @var string $name The name of the object
+     */
     protected string $name = 'link';
 
+    /**
+     * @var array<"value"|"rel"|"href"|"hreflang"|"title"|"media"|"type">
+     */
     protected array $allowedKeys = [
         'value',
         'rel',
@@ -21,62 +30,120 @@ class Link extends AbstractRdapResponseDataRecursiveArray
         'type',
     ];
 
+    /**
+     * @var array{
+     *     value?: Value,
+     *     rel?: Rel,
+     *     href?: Href,
+     *     hreflang?: HrefLang,
+     *     title?: Title,
+     *     media?: Media,
+     *     type?: Type,
+     * } $values
+     */
+    protected array $values = [];
+
+    /**
+     * @param Value|Rel|Href|HrefLang|Title|Media|Type ...$args
+     */
     public function __construct(Value|Rel|Href|HrefLang|Title|Media|Type ...$args)
     {
-        $this->values = [
-            'value' => null,
-            'rel' => null,
-            'href' => null,
-            'hreflang' => null,
-            'title' => null,
-            'media' => null,
-            'type' => null
-        ];
+        $this->values = [];
         foreach ($args as $arg) {
-            $name = $arg->getName();
-            if (!in_array($name, $this->allowedKeys)) {
+            if ($arg instanceof Rel) {
+                $this->values['rel'] = $arg;
                 continue;
             }
-            $this->values[$name] = $arg;
+            if ($arg instanceof HrefLang) {
+                $this->values['hreflang'] = $arg;
+                continue;
+            }
+            if ($arg instanceof Title) {
+                $this->values['title'] = $arg;
+                continue;
+            }
+            if ($arg instanceof Media) {
+                $this->values['media'] = $arg;
+                continue;
+            }
+            if ($arg instanceof Type) {
+                $this->values['type'] = $arg;
+                continue;
+            }
+            if ($arg instanceof Href) {
+                $this->values['href'] = $arg;
+                continue;
+            }
+            $this->values['value'] = $arg;
         }
-        $this->values = array_filter($this->values);
     }
 
+    /**
+     * @return array<string, Value|Rel|Href|HrefLang|Title|Media|Type>
+     */
     public function getValues(): array
     {
         return array_filter($this->values);
     }
 
+    /**
+     * Get the value
+     * @return Value|null
+     */
     public function getValue() : ?Value
     {
         return $this->values['value']??null;
     }
 
+    /**
+     * Get the rel
+     * @return Rel|null
+     */
     public function getRel() : ?Rel
     {
         return $this->values['rel']??null;
     }
 
+    /**
+     * Get the href
+     * @return Href|null
+     */
     public function getHref() : ?Href
     {
         return $this->values['href']??null;
     }
 
+    /**
+     * Get the hreflang
+     * @return HrefLang|null
+     */
     public function getHrefLang() : ?HrefLang
     {
         return $this->values['hreflang']??null;
     }
 
+    /**
+     * Get the title
+     * @return Title|null
+     */
     public function getTitle() : ?Title
     {
         return $this->values['title']??null;
     }
 
+    /**
+     * Get the media
+     * @return Media|null
+     */
     public function getMedia() : ?Media
     {
         return $this->values['media']??null;
     }
 
+    /**
+     * Get the type
+     * @return Type|null
+     */
     public function getType() : ?Type
     {
         return $this->values['type']??null;
