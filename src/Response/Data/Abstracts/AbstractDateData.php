1110e9ce0c6d5b980d1bae000f80f8cc99baa780
ArrayIterator <arrayiterator@gmail.com>
2025-01-03 18:30:45 +0700

    - add doc comment for documentation phpstan improvement - casting Stringable DateTimeImmutable

--- src/Response/Data/Abstracts/AbstractDateData.php
+++ src/Response/Data/Abstracts/AbstractDateData.php
@@ -15,15 +15,21 @@ abstract class AbstractDateData implements RdapResponseDataStringableInterface
 {
     use AllowedKeyDataTraits;
 
+    /**
+     * @var string $name data name
+     */
     protected string $name;
 
+    /**
+     * @var DateTimeInterface $values date data
+     */
     protected DateTimeInterface $values;
 
     public function __construct(string|Stringable|DateTimeInterface $data)
     {
         if (!$data instanceof DateTimeInterface) {
             /** @noinspection PhpUnhandledExceptionInspection */
-            $data = new DateTimeImmutable($data);
+            $data = new DateTimeImmutable((string) $data);
         }
         if (!$data instanceof DateTimeImmutable) {
             $data = DateTimeImmutable::createFromInterface($data);
@@ -32,41 +38,71 @@ abstract class AbstractDateData implements RdapResponseDataStringableInterface
         $this->values = $data;
     }
 
+    /**
+     * @inheritDoc
+     * @return bool
+     */
     public function rootOnly() : bool
     {
         return false;
     }
 
+    /**
+     * @inheritDoc
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * Get date
+     * @return DateTimeInterface
+     */
     public function getDate(): DateTimeInterface
     {
         return $this->values;
     }
 
+    /**
+     * @inheritDoc
+     * @return string
+     */
     public function getValues(): string
     {
         return $this->getStringData();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getStringData(): string
     {
         return $this->values->format(RdapServiceInterface::DATE_FORMAT);
     }
 
+    /**
+     * @return string
+     */
     public function __toString(): string
     {
         return $this->getStringData();
     }
 
+    /**
+     * @inheritDoc
+     * @return string
+     */
     public function jsonSerialize(): string
     {
         return $this->getStringData();
     }
 
+    /**
+     * @inheritDoc
+     * @return string
+     */
     public function getPlainData(): string
     {
         return $this->getStringData();
